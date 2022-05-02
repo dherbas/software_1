c280efc4cdb8311d96913a57e9f29f0ff3d5dbd2
Diego Herbas <dherbas@grupopaz.com.bo>
2022-05-02 03:11:07 -0400

    modificacion

--- src/app/models/Session.ts
+++ src/app/models/Session.ts
@@ -1,7 +1,7 @@
+import { User } from 'src/app/models/user';
 // import {User} from "./user";
 import { Tokens } from './Tokens';
 import { cmbService } from './cmbService';
-import { User } from './user';
 
 export interface Session {
   user: User;
